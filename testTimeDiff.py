faf60737639d99dd5c59b9ee295399c678900313
bowenchancea <87623843+bowenchancea@users.noreply.github.com>
2021-07-19 10:28:44 -0400

    Update testTimeDiff.py

--- testTimeDiff.py
+++ testTimeDiff.py
@@ -1,8 +1,19 @@
 import timeDiff
+import time
 
-testDate = (2022,1,1,0,0,0)
+#declaring variables for testing
+testDate = timeDiff.makeDate(2022, 1, 1, 0, 0, 0)
+now = timeDiff.now()
 
-print(timeDiff.rD(2022,1,1,0,0,0))
-print(timeDiff.rH(2022,1,1,0,0,0))
-print(timeDiff.rM(2022,1,1,0,0,0))
-print(timeDiff.rS(2022,1,1,0,0,0))
+#basic testing
+print(timeDiff.rD(testDate, now))
+print(timeDiff.rH(testDate, now))
+print(timeDiff.rM(testDate, now))
+print(timeDiff.rS(testDate, now))
+
+#making a countdown clock
+while 0 == 0: 
+    now = timeDiff.now()
+    print(timeDiff.rD(testDate, now), "days,", timeDiff.rH(testDate, now), "hours,",
+    timeDiff.rM(testDate, now), "minutes, and", timeDiff.rS(testDate, now), "seconds remaining")
+    time.sleep(1)
